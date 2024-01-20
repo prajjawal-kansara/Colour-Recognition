00c4f0f87858e0dcbecda15cf347cc1960d0f9da
prajjawal-algos <prajjawalkansara@gmail.com>
2024-01-21 01:48:45 +0530

    commit

--- colour_recognition.py
+++ colour_recognition.py
@@ -42,6 +42,27 @@ while(1):
     white_upper = np.array([180, 30, 255], np.uint8) 
     white_mask = cv2.inRange(hsvFrame, white_lower, white_upper) 
 
+     # Set range for yellow color and define mask 
+    yellow_lower = np.array([20, 100, 100], np.uint8) 
+    yellow_upper = np.array([30, 255, 255], np.uint8) 
+    yellow_mask = cv2.inRange(hsvFrame, yellow_lower, yellow_upper) 
+
+    
+    # Set range for purple color and define mask
+    purple_lower = np.array([125, 50, 40], np.uint8)
+    purple_upper = np.array([155, 255, 255], np.uint8)
+    purple_mask = cv2.inRange(hsvFrame, purple_lower, purple_upper)
+
+    # Set range for orange color and define mask
+    orange_lower = np.array([10, 100, 100], np.uint8)
+    orange_upper = np.array([20, 255, 255], np.uint8)
+    orange_mask = cv2.inRange(hsvFrame, orange_lower, orange_upper)
+
+    # Set range for cyan color and define mask
+    cyan_lower = np.array([85, 100, 100], np.uint8)
+    cyan_upper = np.array([100, 255, 255], np.uint8)
+    cyan_mask = cv2.inRange(hsvFrame, cyan_lower, cyan_upper)
+
     # Morphological Transform, Dilation for each color and bitwise_and operator 
     # between imageFrame and mask determines to detect only that particular color 
     kernel = np.ones((5, 5), "uint8") 
@@ -66,15 +87,39 @@ while(1):
     white_mask = cv2.dilate(white_mask, kernel) 
     res_white = cv2.bitwise_and(imageFrame, imageFrame, mask=white_mask) 
 
-    # Process contours for each color
+    # For yellow color 
+    yellow_mask = cv2.dilate(yellow_mask, kernel) 
+    res_yellow = cv2.bitwise_and(imageFrame, imageFrame, mask=yellow_mask) 
+
+     # For purple color
+    purple_mask = cv2.dilate(purple_mask, kernel)
+    res_purple = cv2.bitwise_and(imageFrame, imageFrame, mask=purple_mask)
+
+    # For orange color
+    orange_mask = cv2.dilate(orange_mask, kernel)
+    res_orange = cv2.bitwise_and(imageFrame, imageFrame, mask=orange_mask)
+
+    # For cyan color
+    cyan_mask = cv2.dilate(cyan_mask, kernel)
+    res_cyan = cv2.bitwise_and(imageFrame, imageFrame, mask=cyan_mask)
+
+
+
+    # Process contours for each color in BGR formate
     process_contours(imageFrame, red_mask, (0, 0, 255), "Red Colour")
     process_contours(imageFrame, green_mask, (0, 255, 0), "Green Colour")
     process_contours(imageFrame, blue_mask, (255, 0, 0), "Blue Colour")
     process_contours(imageFrame, black_mask, (0, 0, 0), "Black Colour")
     process_contours(imageFrame, white_mask, (255, 255, 255), "White Colour")
+    process_contours(imageFrame, yellow_mask, (0, 255, 255), "Yellow Colour")
+    process_contours(imageFrame, purple_mask, (128, 0, 128), "Purple Colour")
+    process_contours(imageFrame, orange_mask, (0, 165, 255), "Orange Colour")
+    process_contours(imageFrame, cyan_mask, (255, 255, 0), "Cyan Colour")
 
     # Display the result 
     cv2.imshow("Multiple Color Detection in Real-Time", imageFrame) 
+
+    #break the loop when 'q' key is pressed
     if cv2.waitKey(10) & 0xFF == ord('q'): 
         webcam.release() 
         cv2.destroyAllWindows() 
